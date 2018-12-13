49e60b58982d81626746b6ce1ebcbd9d55df1b01
wangar <wangar@yomob.com>
2018-12-13 18:42:20 +0800

    leetcode121 dfs

---
+++ LeetCode/src/code/lc112_DFS.java
@@ -0,0 +1,25 @@
+package code;
+
+
+/**
+ * Given a binary tree and a sum,
+ * determine if the tree has a root-to-leaf path such
+ * that adding up all the values along the path equals the given sum.
+ *
+ * Note: A leaf is a node with no children.
+ */
+public class lc112_DFS {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        sum -= root.val;
+        if(root.left == null && root.right == null) {
+            return sum == 0;
+
+        } else {
+            return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        }
+    }
+}
+
