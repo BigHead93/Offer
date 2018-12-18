6bdb40e942c187fb249af88f50b123db04ba2086
wangar <wangar@yomob.com>
2018-12-18 14:15:34 +0800

    lc121 max profit

--- LeetCode/src/code/lc121.java
+++ LeetCode/src/code/lc121.java
@@ -1,4 +1,23 @@
 package code;
 
 public class lc121 {
+    public static int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 2)
+            return 0;
+        int low = prices[0], high = prices[0], max = 0;
+        for (int i : prices) {
+            if(i < low)
+                low = high = i;
+            if(i > high) {
+                high = i;
+                max = high - low > max ? high - low : max;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
 }
