a34a99c752d8fcfad725b4374bf40344940fedb3
BigHead93 <458390849@qq.com>
2019-01-03 17:14:08 +0800

    lc110 tell if a tree is a balanced binary tree

---
+++ LeetCode/src/code/lc110_BalancedBinaryTree.java
@@ -0,0 +1,27 @@
+package code;
+
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ */
+public class lc110_BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+            return true;
+        if(Math.abs(depth(root.left)-depth(root.right))>1)
+            return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int depth(TreeNode root) {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return 1;
+        return Math.max(depth(root.left), depth(root.right)) +1;
+    }
+}
